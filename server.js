490ab0157008bb4728c6c764022b091daf744c58
dwleonhardt <dwleonhardt@gmail.com>
2017-05-30 13:55:12 -0600

    all work

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
+app.use(cookieParser());
+
 
 app.get('/set', (req,res,next) => {
-  
+  res.cookie('foo', 'bar');
+  res.send();
 });
 
 app.get('/read', (req,res,next) => {
-
+  var recieved = cookieParser.JSONCookies(req.cookies);
+  res.send(recieved);
 });
 
 app.get('/clear', (req,res,next) => {
-
+  res.clearCookie('deleteMe');
+  res.send();
 });
 
 app.listen('3000', () => {
